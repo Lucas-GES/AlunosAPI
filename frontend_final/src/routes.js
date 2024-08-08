da491e2dd3c879e1276b10202e1adda4427062e9
Lucas Santo <lucas.santo@elitesoft.com.br>
2024-08-07 23:26:24 -0300

    ASP .NET Core - Aula 14

--- frontend_final/src/routes.js
+++ frontend_final/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Alunos from './pages/Alunos';
+import NovoAluno from './pages/NovoAluno';
 
 export default function Rotas(){
     return (
@@ -9,6 +10,7 @@ export default function Rotas(){
             <Routes>
                 <Route path='/' exact Component={Login}/>
                 <Route path='/alunos' Component={Alunos}/>
+                <Route path='/alunos/aluno/novo/:alunoId' Component={NovoAluno}/>
             </Routes>
         </BrowserRouter>
     );
